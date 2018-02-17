1fa8c6ff4d58cecb089ac80b8e1fba38b3343f97
guruwanabe <bp.stanciu@gmail.com>
2018-02-17 16:13:34 +0200

    Update actionFeedback.js

--- actionFeedback.js
+++ actionFeedback.js
@@ -7,77 +7,76 @@ function ActionFeedBack($element, $timeout){
 
 ActionFeedBack.prototype = {
 	setListener: function($timeout){
-  
-      if('ontouchstart' in window || navigator.maxTouchPoints){
-           this.eventHandler = 'touchstart'
-      }
-        
-        this.element.on(this.eventHandler, function(event){
-            var self = $(this);
-            /*if (t.data('feedback-loading') == true) {
-                return; // do nothing
-            }*/
-            this.toggleFeedback(self, event, true);
-            if($.support.transition){
-                self.one('bsTransitionEnd', function () {
-                    toggleFeedback(self, event, false);
-                }).emulateTransitionEnd(this.timeout);
-            }else{
-               setTimeout(function(){
-                   toggleFeedback(self, event, false);
-               }, (this.timeout))
-            }
-        });
-    },
-    toggleFeedback: function(element, event, $state){
-        var position = getEventPosition(event, 15);
-        if($state){
-            this.createFeedBackElement(position.timestamp);
-            element.data('feedback-loading', $state);
-            element.addClass('feedback-active');
-            element.attr('aria-pressed', $state);
-        }else{
-            element.removeClass('feedback-active');
-            element.attr('aria-pressed', false);
-            element.data('feedback-loading', false);
-            element.attr('style', '');
-            this.removeFeedBackElement(position.timestamp);
-        }
-    },
+		if('ontouchstart' in window || navigator.maxTouchPoints){
+		   this.eventHandler = 'touchstart'
+		}
+
+		this.element.on(this.eventHandler, function(event){
+			var self = $(this);
+			/*if (t.data('feedback-loading') == true) {
+			return; // do nothing
+			}*/
+			this.toggleFeedback(self, event, true);
+			if($.support.transition){
+				self.one('bsTransitionEnd', function () {
+				    toggleFeedback(self, event, false);
+				}).emulateTransitionEnd(this.timeout);
+			}else{
+				setTimeout(function(){
+				   toggleFeedback(self, event, false);
+				}, (this.timeout))
+			}
+		});
+	},
+	toggleFeedback: function(element, event, $state){
+		var position = getEventPosition(event, 15);
+		if($state){
+		    this.createFeedBackElement(position.timestamp);
+		    element.data('feedback-loading', $state);
+		    element.addClass('feedback-active');
+		    element.attr('aria-pressed', $state);
+		}else{
+		    element.removeClass('feedback-active');
+		    element.attr('aria-pressed', false);
+		    element.data('feedback-loading', false);
+		    element.attr('style', '');
+		    this.removeFeedBackElement(position.timestamp);
+		}
+	},
 	setPosition: function(el, $x, $y){
-        return el.css({
-            'top': $y,
-            'left':$x,
-            'width': 50,
-            'height': 50,
-            'position': 'absolute',
-            'z-index': 1060
-        })
-    },
-    getEventPosition: function($event, $offset) {
+		return el.css({
+		    'top': $y,
+		    'left':$x,
+		    'width': 50,
+		    'height': 50,
+		    'position': 'absolute',
+		    'z-index': 1060
+		})
+	},
+	getEventPosition: function($event, $offset) {
 	    $offset = ($offset || 0);
 	    var event = $event;
-	    
+
 	    if('ontouchstart' in window || navigator.maxTouchPoints){
-		var eventScreen = $event.originalEvent.touches[0];
-		event.pageX = eventScreen.pageX;
-		event.pageY = eventScreen.pageY
+			var eventScreen = $event.originalEvent.touches[0];
+			event.pageX = eventScreen.pageX;
+			event.pageY = eventScreen.pageY
 	    }
 	    return {
-	        x: (event.pageX) - $offset,
-	        y: (event.pageY) - $offset,
-	        timestamp: event.timeStamp
+			x: (event.pageX) - $offset,
+			y: (event.pageY) - $offset,
+			timestamp: event.timeStamp
 	    }
 	}, 
 	createFeedBackElement(timestamp){
 		var clickArea = document.createElement('div');
-			  clickArea.setAttribute('id', timestamp);
-        clickArea.setAttribute('data-click-feedback', '');
-        clickArea.setAttribute('class', 'feedback-active');
+		clickArea.setAttribute('id', timestamp);
+		clickArea.setAttribute('data-click-feedback', '');
+		clickArea.setAttribute('class', 'feedback-active');
 
-        objBody.append(clickArea);
+		objBody.append(clickArea);
 
-        return setPosition($(clickArea), position.x, position.y);
+		return setPosition($(clickArea), position.x, position.y);
 	},
 	removeFeedBackElement(timestamp){
 		return document.getElementById(timestamp).remove();
